6b275f8e4458f1abc3c503e1535c8e89eba0974a
Valery Jacobs <valeryjacobs@hotmail.com>
2014-06-08 13:34:52 +0200

    Init

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string stateInput)
+        {
+            Clients.All.updateState(stateInput);
+        }
+    }
+}
